d291619bd950b2f69335a002105d6a65cb122ec0
CarlosRoberto2012305 <carlosfranco.2a2012@gmail.com>
2020-12-07 14:46:51 -0600

    numeros en incremento en forma de piramide y pino

--- TAREA/src/main/java/Figuras.java
+++ TAREA/src/main/java/Figuras.java
@@ -13,5 +13,32 @@ public class Figuras{
         }
         System.out.print("dimension del arbol:\t");
         int b= input.nextInt();
+        for (int y=0;y<=b;y++){
+            for(int x=1;x<=b-y;x++){
+                System.out.print(" ");
+            }
+            for(int w=1;w<=y;w++){
+                System.out.print("*");
+            }
+            for (int z=0;z<=y;z++){
+                    System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int th = 0; th<=(b/3);th++){
+            for(int va=0; va<=(b/2); va++ ){
+                System.out.print(" ");
+
+            }
+            for(int tb=0; tb<=(b/2); tb++ ){
+                System.out.print("*");
+            }
+            for(int tbb=0; tbb<=(b/3); tbb++ ){
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+
     }
 }
